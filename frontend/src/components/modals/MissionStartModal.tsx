68e53c1ea2aafe504e99b51efda7f261d32c7fcd
FORSAA <EgorZaharov51@yandex.ru>
2026-02-03 00:07:25 +0300

    minor fixes

--- frontend/src/components/modals/MissionStartModal.tsx
+++ frontend/src/components/modals/MissionStartModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { AlertTriangle, Check, ChevronDown, X } from 'lucide-react';
 import { AppContext } from '../../context/AppContext';
 
